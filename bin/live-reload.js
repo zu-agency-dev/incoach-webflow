5c28cd04f67687ffb8d8d8d4937c2ce83b12db80
Dariusz Sułtanowski <sultanowski.d@gmail.com>
2025-01-19 22:09:36 +0100

    config: add /dist files and change configuration

--- bin/live-reload.js
+++ bin/live-reload.js
@@ -1,1 +1,1 @@
-new EventSource(`${SERVE_ORIGIN}/esbuild`).addEventListener('change', () => location.reload());
+// new EventSource(`${SERVE_ORIGIN}/esbuild`).addEventListener('change', () => location.reload());
